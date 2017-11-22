c07349e361c68557e5883c5b55d776d64cfad7bc
Allyson Halley <isolation85@gmail.com>
2017-11-22 13:34:06 -0300

    Estabilização de tela, js, css

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :hierarchies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'home', to: 'home#index'
-
-  root to: 'home#index'
+  root to: 'hierarchies#index'
 
 end
